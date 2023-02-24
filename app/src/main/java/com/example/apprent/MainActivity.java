f9abdb3896ec82ee433f9152bc7a16552bd4ab27
Gabriel Garbage <gabrielg@oa-b4.msk.21-school.ru>
2023-02-24 21:31:49 +0300

    exercise 6: second Activity, data exchange

--- app/src/main/java/com/example/apprent/MainActivity.java
+++ app/src/main/java/com/example/apprent/MainActivity.java
@@ -2,29 +2,41 @@ package com.example.apprent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String TAG=this.getClass().getSimpleName();
-    Button buttonSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonSignUp=findViewById(R.id.signUp);
-        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+        boolean signIn=false;
+        Bundle arguments=getIntent().getExtras();
+        if(arguments!=null){
+            signIn=arguments.getBoolean("signin");
+        }
+        TextView textView=findViewById(R.id.textView3);
+        if(signIn){
+            textView.setText("Вход выполнен");
+        }else{
+            textView.setText("Вход не выполнен");
+        }
+        Button button=findViewById(R.id.button);
+        boolean finalSignIn = signIn;
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "Sign in (setOnClickListener)");
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                intent.putExtra("signin", finalSignIn);
+                startActivity(intent);
+                finish();
             }
         });
     }
-    public void forExample(View view){
-        Log.i(TAG,"Sign up (attribute onClick)");
-    }
-
 }
