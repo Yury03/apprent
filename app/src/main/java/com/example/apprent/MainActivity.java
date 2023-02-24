2726b88dcc7401b03f4b9d80c04f02039e386875
Gabriel Garbage <gabrielg@oa-b4.msk.21-school.ru>
2023-02-24 20:00:30 +0300

    exercise 4: buttons, listener, attribute onClick

--- app/src/main/java/com/example/apprent/MainActivity.java
+++ app/src/main/java/com/example/apprent/MainActivity.java
@@ -4,56 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG=this.getClass().getSimpleName();
+    Button buttonSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
-        callLog("onCreate");
-
+        buttonSignUp=findViewById(R.id.signUp);
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Sign in (setOnClickListener)");
+            }
+        });
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
-        callLog("onStart");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
-        callLog("onResume");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
-        callLog("onPause");
+    public void forExample(View view){
+        Log.i(TAG,"Sign up (attribute onClick)");
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
-        callLog("onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
-callLog("onDestroy");
-    }
-    private void callLog(String method){
-        Log.e(TAG,method);
-        Log.w(TAG, method);
-        Log.i(TAG,method);
-        Log.d(TAG, method);
-        Log.v(TAG, method);
-    }
 }
