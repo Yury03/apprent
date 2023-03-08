9435f4fa6305cea11c7c6c0b1d040a9e137e602e
Gabriel Garbage <gabrielg@oa-b4.msk.21-school.ru>
2023-03-08 21:15:09 +0300

    fixed 8.3.23

--- app/src/main/java/com/example/apprent/LoginFragment.java
+++ app/src/main/java/com/example/apprent/LoginFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,43 +16,46 @@ import android.widget.Toast;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class LoginFragment extends Fragment {
-    Bundle resultBundle = new Bundle();
-    AtomicBoolean signIn;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        signIn = new AtomicBoolean(false);
-        getParentFragmentManager()
-                .setFragmentResultListener("requestKey", this,
-                        ((requestKey, result) -> {
-                            signIn.set(resultBundle.getBoolean("isSignIn"));
-                        }));
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        Button signInButton = view.findViewById(R.id.signIn);
-        Button signUpButton = view.findViewById(R.id.signUp);
-        signInButton.setOnClickListener(v -> {
-            signIn.set(!(signIn.get()));
-            Toast.makeText(this.getContext(), signIn.toString(), Toast.LENGTH_LONG).show();
-        });
-        signUpButton.setOnClickListener(view1 -> {
-            resultBundle.putBoolean("isSignIn", signIn.get());
-            getParentFragmentManager().setFragmentResult(
-                    "requestKey", resultBundle);
-            Fragment fragment = new MainFragment();
-            getParentFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragmentContainerView, fragment)
-                    .commit();
-        });
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
-    }
+	Boolean signIn;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+	
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		Button signInButton = view.findViewById(R.id.signIn);
+		Button signUpButton = view.findViewById(R.id.signUp);
+		getParentFragmentManager().setFragmentResultListener("request",
+				this, (requestKey, result) -> {
+					signIn = result.getBoolean("isSignIn");
+					
+				});
+		signInButton.setOnClickListener(v -> {
+			signIn = !(signIn);
+			Toast.makeText(this.getContext(), signIn.toString(), Toast.LENGTH_LONG).show();
+		});
+		signUpButton.setOnClickListener(view1 -> {
+			Bundle bundle = new Bundle();
+			bundle.putBoolean("isSignIn2", signIn);
+			getParentFragmentManager().setFragmentResult(
+					"request2", bundle);
+			Fragment fragment = new MainFragment();
+			getParentFragmentManager()
+					.beginTransaction()
+					.setReorderingAllowed(true)
+					.replace(R.id.fragmentContainerView, fragment)
+					.commit();
+		});
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_login, container, false);
+	}
 }
--- app/src/main/java/com/example/apprent/MainFragment.java
+++ app/src/main/java/com/example/apprent/MainFragment.java
@@ -5,53 +5,62 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainFragment extends Fragment {
-    AtomicBoolean signIn;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        signIn = new AtomicBoolean(true);
-        getParentFragmentManager().setFragmentResultListener("requestKey",
-                this,
-                (requestKey, bundle) -> signIn.set(bundle.getBoolean("isSignIn")));
+	Boolean signIn=false;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		Button button = view.findViewById(R.id.button2);
+		TextView textView = view.findViewById(R.id.textView4);
+		getParentFragmentManager().setFragmentResultListener("request2",
+				this, (requestKey, result) -> {
+					signIn = result.getBoolean("isSignIn2");
+					Toast.makeText(this.getContext(), signIn.toString(), Toast.LENGTH_LONG).show();
+					if (signIn) {
+						textView.setText("Вход выполнен");
+					} else {
+						textView.setText("Вход не выполнен");
+					}
+				});
 
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        Button button = view.findViewById(R.id.button2);
-        TextView textView = view.findViewById(R.id.textView4);
-
-        if (signIn.get()) {
-            textView.setText("Вход выполнен");
-        } else {
-            textView.setText("Вход не выполнен");
-        }
-
-        button.setOnClickListener(view1 -> {
-            Fragment fragment = new LoginFragment();
-            getParentFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragmentContainerView, fragment)
-                    .commit();
-        });
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+		
+		button.setOnClickListener(view1 -> {
+			Bundle bundle = new Bundle();
+			bundle.putBoolean("isSignIn", signIn);
+			getParentFragmentManager().setFragmentResult(
+					"request", bundle);
+			Fragment fragment = new LoginFragment();
+			getParentFragmentManager()
+					.beginTransaction()
+					.setReorderingAllowed(true)
+					.replace(R.id.fragmentContainerView, fragment)
+					.commit();
+		});
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_main, container, false);
+	}
 }
